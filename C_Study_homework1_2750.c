6ba9f0e1239bb0e0c17b785f86e94b70f24d4744
chjpooh2002 <121940770+chjpooh2002@users.noreply.github.com>
2023-01-04 21:04:22 +0900

    Update C_Study_homework1_2750.c

--- C_Study_homework1_2750.c
+++ C_Study_homework1_2750.c
@@ -13,8 +13,8 @@ int main(void)
 	for(i=0;i<n-1;i++)
 	{
 		minimum = i;//i번째 원소를 최소라 생각하고 아래 for문에서
-					//i번째 이후의 모든 원소에 접근하여 이 중 최소값을 갖는
-					// 원소의 인덱스 값을 minimum에 저장
+			    //i번째 이후의 모든 원소에 접근하여 이 중 최소값을 갖는
+			    // 원소의 인덱스 값을 minimum에 저장
 		for(j=i+1;j<n;j++)
 		{
 			if(number[minimum]>number[j]) 
